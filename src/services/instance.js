4f41c5ba196f5d8464f29cfab148af3a164a6793
sathish-RG <sathish958527@gmail.com>
2025-04-21 18:23:04 +0530

    one-more

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,28 +1,14 @@
 import axios from 'axios';
 
-const baseURL = 'https://be-ecommerce-v1.onrender.com/api/v1';
+const baseURL= 'https://be-ecommerce-v1.onrender.com/api/v1'
 
-const instance = axios.create({
+const instance=axios.create({
   baseURL,
-  timeout: 10000,
-  headers: {
-    'Content-Type': 'application/json',
+  timeout:10000,
+  headers:{
+    'Content-type':'application/json',
+    
   },
-  withCredentials: true,
+  withCredentials:true,
 });
-
-// Request Interceptor to add Authorization header
-instance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token'); // or from cookies or any storage
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
 export default instance;
